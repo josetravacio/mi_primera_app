1c1dc2ad97c2893a278a15bf45af3dd8b5aba465
jose <josejesusmt@yahoo.com.ar>
2023-11-21 08:33:33 -0300

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton: Button = findViewById(R.id.button)
+        val myTextView: TextView = findViewById(R.id.textView)
+
+        myButton.setOnClickListener {
+            // Si el TextView está visible, ocúltalo; de lo contrario, muéstralo
+            myTextView.visibility = if (myTextView.visibility == View.VISIBLE) {
+                View.INVISIBLE
+            } else {
+                View.VISIBLE
+            }
+        }
     }
 }
